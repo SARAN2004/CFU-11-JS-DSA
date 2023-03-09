82829bc7a80cbbdd7749dc202963ae05c5014e72
SARAN2004 <116723092+SARAN2004@users.noreply.github.com>
2023-03-09 18:37:15 +0530

    Add files via upload

---
+++ assets/JS/app.js
@@ -0,0 +1,13 @@
+
+/*get the number of characters*/
+function charLength() {
+
+    let charLength = document.getElementById('length').value;
+    document.getElementById('result').innerHTML = charLength;
+    return charLength;
+
+}
+
+
+
+
